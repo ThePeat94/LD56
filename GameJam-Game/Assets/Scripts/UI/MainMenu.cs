8649a5f47b41fdb2c6ff143e5b1a1256a657bf1e
Peat <hendy123@gmx.de>
2024-10-07 21:52:34 +0200

    add game transition add shiver animation adjust buttons

--- GameJam-Game/Assets/Scripts/UI/MainMenu.cs
+++ GameJam-Game/Assets/Scripts/UI/MainMenu.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -12,7 +14,9 @@ namespace Nidavellir.UI
         [SerializeField] private Slider m_sfxVolumeSlider;
         [SerializeField] private GameObject m_optionsPanel;
 
-
+        [SerializeField] private GameObject m_gameTransition;
+        [SerializeField] private GameObject m_shiverImage;
+        
         private void Awake()
         {
             this.m_musicVolumeSlider.onValueChanged.AddListener(this.MusicVolumeSliderChanged);
@@ -23,6 +27,8 @@ namespace Nidavellir.UI
         {
             this.m_musicVolumeSlider.value = GlobalSettings.Instance.MusicVolume;
             this.m_sfxVolumeSlider.value = GlobalSettings.Instance.SfxVolume;
+            
+            this.m_gameTransition.SetActive(false);
         }
 
         public void BackFromCreditsToStart()
@@ -71,6 +77,16 @@ namespace Nidavellir.UI
 
         public void StartGame()
         {
+            this.m_gameTransition.SetActive(true);
+            this.m_startMenu.SetActive(false);
+            StartCoroutine(this.StartGameDelayed());
+            this.m_shiverImage.transform.DOShakePosition(1f, 5)
+                .SetLoops(-1);
+        }
+
+        private IEnumerator StartGameDelayed()
+        {
+            yield return new WaitForSeconds(4);
             SceneManager.LoadScene("mainGameScene");
         }
     }
